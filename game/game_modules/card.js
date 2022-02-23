d8a47558fb65c357329026ea7a0c5f57354ca1ec
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-02-23 20:03:22 +0100

    switch delle carte fatto

--- game/game_modules/card.js
+++ game/game_modules/card.js
@@ -1,54 +1,80 @@
 class Card {
+
     constructor(forma, colore ) {
-    this.forma = forma;
-    this.colore = colore;
+        this.forma = forma;
+        this.colore = colore;
     }
 
     show(){
+
         let card = document.createElement('div')
-        card.classList.add('card-front', 'd-flex', 'justify-content-center', 'align-items-center')
+        card.classList.add('card', 'card-back', 'd-flex', 'justify-content-center', 'align-items-center')
 
-        let shape = document.createElement('div')
+        let cardLogo = document.createElement('div')
+        cardLogo.classList.add('card-logo')
 
-        let blu = "#0080fe"
-        let giallo = "#da833b"
-        let verde = "#DAA520"
-        let rosso = "#de0000"
-        let viola = "#4a2356"
-
-
-        if(this.forma == "cerchio"){
-        shape.classList.add('cerchio')
-        } else {
-            shape.classList.add('rombo')
-        }
-
-        if(this.colore == "blu"){
-            card.style.borderColor = blu
-            shape.style.backgroundColor = blu
-        }
-        if(this.colore == "verde"){
-            card.style.borderColor = verde
-            shape.style.backgroundColor = verde
-        }
-        if(this.colore == "rosso"){
-            card.style.borderColor = rosso
-            shape.style.backgroundColor = rosso
-        }
-        if(this.colore == "viola"){
-            card.style.borderColor = viola
-            shape.style.backgroundColor = viola
-        }
-        if(this.colore == "giallo"){
-            card.style.borderColor = giallo
-            shape.style.backgroundColor = giallo
-        }
-
-
-        card.append(shape)
+        card.append(cardLogo)
         board.append(card)
+        
+
+        card.addEventListener('click', ()=>{
+
+            card.classList.toggle('card-back')
+            card.classList.toggle('card-front')
+            cardLogo.classList.toggle('card-logo')
+  
+            if(this.forma == "cerchio"){
+                cardLogo.classList.toggle('cerchio')
+            }
+
+            if(this.forma == "rombo"){
+                cardLogo.classList.toggle('rombo')
+            }
+    
+            if(this.colore == "blu"){
+                card.classList.toggle('border-blu')
+                cardLogo.classList.toggle('shape-blu')
+            }
+
+            if(this.colore == "verde"){
+                card.classList.toggle('border-verde')
+                cardLogo.classList.toggle('shape-verde')
+            }
+
+            if(this.colore == "rosso"){
+                card.classList.toggle('border-rosso')
+                cardLogo.classList.toggle('shape-rosso')
+            }
+
+            if(this.colore == "viola"){
+                card.classList.toggle('border-viola')
+                cardLogo.classList.toggle('shape-viola')
+            }
+
+            if(this.colore == "giallo"){
+                card.classList.toggle('border-giallo')
+                cardLogo.classList.toggle('shape-giallo')
+            }
+
+        })
+
 
     }
+
+    // showBack(){
+
+    //     let cardBack = document.createElement('div')
+    //     cardBack.classList.add('card', 'card-back','d-flex', 'justify-content-center', 'align-items-center')
+
+    //     let cardLogo = document.createElement('div')
+    //     cardLogo.classList.add('card-logo')
+        
+    //     cardBack.append(cardLogo)
+    //     board.append(cardBack)
+
+    // }
+
+    
 
 }
 
--- game/game_modules/game.js
+++ game/game_modules/game.js
@@ -5,59 +5,40 @@ import {Mazzo} from './mazzo.js'
 class Game {
     constructor(){
         
-        this.cardsInMddle =[];
-        this.players =["Comp"];
+        this.cardsInMddle = [];
+        this.players = ["Comp"];
     }
-    
-    // start(p1, p2){
-    //     this.players.push(new Player(p1));
-    //     this.players.push(new Player(p2));
-    //     let mazzo = new Mazzo();
-    //     mazzo.creaMazzo();
-    //     mazzo.mescolaMazzo()
+ 
     //     this.players[0].playerCards = mazzo.cards.slice(0,5);
     //     this.players[1].playerCards = mazzo.cards.slice(5,10);
-    // };
-    
+      
     nuovoGioco(player){
-        
-        
+           
         let mazzo = new Mazzo();
+
         mazzo.creaMazzo();
         mazzo.mescolaMazzo()
+
         this.players.unshift(new Player(player))
         this.cardsInMddle = mazzo
-                    
-            // for (let i=0; i<numGiocatori; i++){
-            //     this.players.push(new Player(prompt("inserisci il nome del giocatore " + (i+1))))    
-            // }
-            
-            // for(let i=0; i<numGiocatori; i++){
-            //     //let start = i*5
-            //     //let end = i*5 +5
-                
-            //     this.players[i].playerCards = mazzo.cards.slice(0, 5);
-            //     for (let j =0; j<5; j++){mazzo.cards.shift()}
-            // }
-
-
-        //console.log(this.players);
-        
-        //console.log(mazzo)
-        console.log(this.cardsInMddle)
+       
+        console.log(typeof this.cardsInMddle.cards)
+        console.log(this.cardsInMddle.cards)
+        //for (let card in mazzo){console.log(card)}
 
         let board = document.createElement('div')
-        //board.style.width = "500"
+        
         board.setAttribute("id", "board")
         board.classList.add("row", "justify-content-center", "board")
         tavolo.append(board)
 
         for(let i=0; i<20; i++){
-            this.cardsInMddle.cards[i].show()
+            this.cardsInMddle.cards[i].show()        
         }
-        // for(let i=5; i<10; i++){
-        //     this.cardsInMddle.cards[i].show()
-        // }
+        
+
+
+        
 
         
     }
